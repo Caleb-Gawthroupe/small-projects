d248a43959859aa6a19517ae0454dfaa4a1bb296
Caleb-Gawthroupe <gawthroupeca@gmail.com>
2023-09-07 15:04:00 -0400

    Create sigdigs.py

---
+++ Significant Digits/sigdigs.py
@@ -0,0 +1,47 @@
+def sigdigs(num, ending):
+    sig = 0
+    num = str(num)
+    trailers = 0
+    
+    if "." in num:
+        decimal = True
+    else:
+        decimal = False
+    if ending == True:
+        decimal = True
+    
+    for char in num:
+        if char == "0":
+            if sig != 0:
+                if decimal == True:
+                    sig += 1
+                else:
+                    trailers += 1         
+        else:
+            if char != '.':
+                sig+=1+trailers
+                trailers = 0
+                 
+        if char == ".":
+            decimal = True
+        
+        
+            
+    
+    
+    print(str(sig) + " Significant Digits")
+
+
+while True:
+    num = input()
+    if num.lower() == "exit":
+        exit()
+    else:
+        try:
+            if num[-1] == ".":
+                num = num[:-1]
+                sigdigs(int(num), ending=True)
+            else:
+                sigdigs(num, ending=False)
+        except:
+            print("Error")
